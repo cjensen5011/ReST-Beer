24d36721a2847cbb6c7b4f0de67855e8a7af8c8f
christopherjensen <christopher.jensen@daugherty.com>
2019-02-06 11:02:28 -0600

    added delete, get, and put routes

--- routes/beerRouter.js
+++ routes/beerRouter.js
@@ -1,17 +1,76 @@
-const express = require('express');
+const express = require("express");
 const beerRouter = express.Router();
+const Beer = require("../models/beer");
 
-beerRouter.get('/', (req, res) => {
-  res.send('Pretend list of beers');
+beerRouter.get("/:beer_id", (req, res) => {
+  Beer.findById(req.params.beer_id, (err, beer) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(beer);
+    }
+  });
 });
 
-beerRouter.post('/', (req, res) => {
-  console.log(req.body);
-  res.send(`Saved your ${req.body.name} to the DB!`);
+beerRouter.put("/:beer_id", (req, res) => {
+  Beer.findById(req.params.beer_id, (err, beer) => {
+    if (err) {
+      res.send(err);
+    }
+
+    beer.name = req.body.name;
+    beer.rating = req.body.rating;
+
+    beer.save((err, document) => {
+      if (err) {
+        res.status(400).send(err);
+      }
+      res.status(200).json(`Beer posted!\n${document}`);
+    });
+  });
 });
 
-beerRouter.use('/', (req, res) => {
-  res.send('Beer router is working!');
+beerRouter.delete("/:beer_id", (req, res) => {
+  Beer.deleteOne(
+    {
+      _id: req.params.beer_id
+    },
+    err => {
+      if (err) {
+        res.send(err);
+      }
+      res.send("You successfully deleted beer: " + req.params.beer_id);
+    }
+  );
+});
+
+beerRouter.get("/", (req, res) => {
+  Beer.find((err, beers) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(beers);
+    }
+  });
+});
+
+// add second router aka breweries and slack it to Matt Huberty
+
+beerRouter.post("/", (req, res) => {
+  let beer = new Beer();
+  beer.name = req.body.name;
+  beer.rating = req.body.rating;
+  beer.save((err, document) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send(`Saved your ${document}.'`);
+    }
+  });
+});
+
+beerRouter.use("/", (req, res) => {
+  res.send("Beer router is working!");
 });
 
 module.exports = beerRouter;
